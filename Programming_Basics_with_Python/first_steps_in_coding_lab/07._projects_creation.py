1b790b16bdd1b1510000fa158e49a3b250d74fa5
Vladimir <v.zapryanov89@gmail.com>
2021-09-10 17:54:23 +0300

    add new item

---
+++ Programming_Basics_with_Python/first_steps_in_coding_lab/07._projects_creation.py
@@ -0,0 +1,5 @@
+architect_name = input()
+project_numbers = int(input())
+required_hours = project_numbers * 3
+print(f"The architect {architect_name} will need {required_hours} hours to complete {project_numbers} project/s.")
+
