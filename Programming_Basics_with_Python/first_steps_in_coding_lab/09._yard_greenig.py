ca8562f18414533a88ceb4638309c5ffe8f7ceef
Vladimir <v.zapryanov89@gmail.com>
2021-09-10 17:55:16 +0300

    add new exercise

---
+++ Programming_Basics_with_Python/first_steps_in_coding_lab/09._yard_greenig.py
@@ -0,0 +1,6 @@
+square_meters = float(input())
+price = square_meters * 7.61
+discount = price * 0.18
+final_price = price - discount
+print(f"The final price is: {final_price} lv.")
+print(f"The discount is: {discount} lv.")
