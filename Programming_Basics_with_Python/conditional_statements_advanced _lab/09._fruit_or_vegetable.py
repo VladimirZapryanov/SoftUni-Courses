4751a844e1b3cb380bd130af4555305dcd69936d
Vladimir <v.zapryanov89@gmail.com>
2021-09-12 16:32:38 +0300

    add new exercise

---
+++ Programming_Basics_with_Python/conditional_statements_advanced _lab/09._fruit_or_vegetable.py
@@ -0,0 +1,9 @@
+name_of_product = input()
+if name_of_product == "banana" or name_of_product == "apple" or name_of_product == "kiwi" \
+        or name_of_product == "cherry" or name_of_product == "lemon" or name_of_product == "grapes":
+    print("fruit")
+elif name_of_product == "tomato" or name_of_product == "cucumber" or name_of_product == "pepper" \
+        or name_of_product == "carrot":
+    print("vegetable")
+else:
+    print("unknown")
