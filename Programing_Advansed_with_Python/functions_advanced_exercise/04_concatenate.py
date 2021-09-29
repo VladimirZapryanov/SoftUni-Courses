b92a8dea1ef91a6061ce6d9d9f32e063a9f6b8fe
Vladimir <v.zapryanov89@gmail.com>
2021-09-29 12:27:10 +0300

    add new exercise

---
+++ Programing_Advansed_with_Python/functions_advanced_exercise/04_concatenate.py
@@ -0,0 +1,13 @@
+def concatenate(*args):
+    result = ""
+    for el in args:
+        result += el
+    return result
+
+
+"""
+some test:
+print(concatenate("Soft", "Uni", "Is", "Great", "!"))
+print(concatenate("I", " ", "Love", " ", "Python"))
+"""
+
