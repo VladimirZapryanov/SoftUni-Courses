5635ffd3685783a421b06e1f34a66a4be9a8a2cf
Vladimir <v.zapryanov89@gmail.com>
2021-10-08 13:41:06 +0300

    add new exercise

---
+++ Programing_Advansed_with_Python/error_hadling_lab/02._value_cannot_be_negative.py
@@ -0,0 +1,20 @@
+class ValueCannotBeNegative(Exception):
+    pass
+
+
+n = 5
+
+for _ in range(n):
+    number = int(input())
+    if number < 0:
+        raise ValueCannotBeNegative
+
+'''
+some test:
+1
+4
+-5
+3
+10
+'''
+
