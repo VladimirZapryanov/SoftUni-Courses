355a6372b7370a2f53b9a4a30dd25f56f243d121
Vladimir <v.zapryanov89@gmail.com>
2021-11-04 20:24:08 +0200

    add new exercise

---
+++ Programming_Fundamentals_with_Python/regular_expressions_lab/03._match_dates.py
@@ -0,0 +1,10 @@
+import re
+
+pattern = r"(?P<day>\d{2})(?P<separator>[\./-])(?P<month>[A-Z][a-z]{2})(?P=separator)(?P<year>\d{4})"
+
+dates = input()
+valid_dates = re.finditer(pattern, dates)
+
+for date in valid_dates:
+    current_date = date.groupdict()
+    print(f"Day: {current_date['day']}, Month: {current_date['month']}, Year: {current_date['year']}")
