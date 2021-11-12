6b8795377266a15b1895efac13d79f02a44ac7da
Vladimir <v.zapryanov89@gmail.com>
2021-11-12 23:37:49 +0200

    add new exercise

---
+++ Programming_Fundamentals_with_Python/function_exercise/01._smallest_of_three_numbers.py
@@ -0,0 +1,8 @@
+def smallest_number(n1, n2, n3):
+    return min(n1, n2, n3)
+
+
+number_1 = int(input())
+number_2 = int(input())
+number_3 = int(input())
+print(smallest_number(number_1, number_2, number_3))
