88387ab4bbeaf0067642985f715e2427ce922679
Vladimir <v.zapryanov89@gmail.com>
2021-11-24 21:46:19 +0200

    add new exercise

---
+++ Programming_Fundamentals_with_Python/list_advanced_exercise/04._number_classification.py
@@ -0,0 +1,11 @@
+numbers = [int(num) for num in input().split(", ")]
+
+positive = [str(num) for num in numbers if num >= 0]
+negative = [str(num) for num in numbers if num < 0]
+even = [str(num) for num in numbers if num % 2 == 0]
+odd = [str(num) for num in numbers if not num % 2 == 0]
+
+print(f"Positive: {', '.join(positive)}")
+print(f"Negative: {', '.join(negative)}")
+print(f"Even: {', '.join(even)}")
+print(f"Odd: {', '.join(odd)}")
