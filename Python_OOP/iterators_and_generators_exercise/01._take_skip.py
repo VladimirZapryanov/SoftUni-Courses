8d9680e1d5586220286482d2fc2fa390459d259b
Vladimir <v.zapryanov89@gmail.com>
2021-12-06 11:26:10 +0200

    add new exercise

---
+++ Python_OOP/iterators_and_generators_exercise/01._take_skip.py
@@ -0,0 +1,27 @@
+class take_skip:
+    def __init__(self, step, count):
+        self.step = step
+        self.count = count
+        self.number = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            num = self.number
+            self.number += self.step
+            if self.count <= 0:
+                raise StopIteration
+
+            self.count -= 1
+            return num
+
+
+numbers = take_skip(2, 6)
+for number in numbers:
+    print(number)
+
+numbers = take_skip(10, 5)
+for number in numbers:
+    print(number)
