c7381bcb07a39de21b08d09a033ed329cd987c8b
Vladimir <v.zapryanov89@gmail.com>
2021-12-18 21:00:43 +0200

    add new exercise

---
+++ Programming_Fundamentals_with_Python/data_types_and_variables_exercise/01._integer_operations.py
@@ -0,0 +1,10 @@
+import math
+
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+num_4 = int(input())
+
+sum_of_number = math.trunc((num_1 + num_2) / num_3) * num_4
+
+print(sum_of_number)
