37a991f90ad4cfa359210475bcd93071abdc7208
VladimirZapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2022-01-20 11:17:56 +0200

    Add files via upload

---
+++ Python_OOP/decorators_exercise/06._html_tags.py
@@ -0,0 +1,28 @@
+import functools
+
+
+def tags(tag):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args):
+            return f"<{tag}>{func(*args)}</{tag}>"
+
+        return wrapper
+    return decorator
+
+
+@tags('p')
+def join_strings(*args):
+    return "".join(args)
+
+
+print(join_strings("Hello", " you!"))
+
+
+@tags('h1')
+def to_upper(text):
+    return text.upper()
+
+
+print(to_upper('hello'))
+
