b297e582e933038a6fd4e21c5e4751dfdf9b9875
VladimirZapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2022-02-17 18:41:46 +0200

    Add files via upload

---
+++ Python_OOP/iterators_and_generators_exercise/02._dictionary_iterator.py
@@ -0,0 +1,26 @@
+class dictionary_iter:
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+        self.keys = list(self.dictionary.keys())
+        self.keys_index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.keys_index >= len(self.keys):
+            raise StopIteration
+        key = self.keys[self.keys_index]
+        value = self.dictionary[key]
+        self.keys_index += 1
+        return key, value
+
+
+result = dictionary_iter({1: "1", 2: "2"})
+for x in result:
+    print(x)
+
+result = dictionary_iter({"name": "Peter", "age": 24})
+for x in result:
+    print(x)
+
