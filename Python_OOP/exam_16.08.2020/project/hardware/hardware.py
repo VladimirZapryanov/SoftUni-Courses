d527de33b9956cbf1696b24fa5482a5ddcb28716
VladimirZapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2022-03-17 14:43:07 +0200

    Add files via upload

---
+++ Python_OOP/exam_16.08.2020/project/hardware/hardware.py
@@ -0,0 +1,26 @@
+from project.software.software import Software
+
+
+class Hardware:
+    def __init__(self, name, hardware_type, capacity, memory):
+        self.name = name
+        self.hardware_type = hardware_type
+        self.capacity = capacity
+        self.memory = memory
+        self.software_components = []
+        self.available_memory = memory
+        self.available_capacity = capacity
+
+    def install(self, software: Software):
+        if self.available_memory < software.memory_consumption or self.available_capacity < software.capacity_consumption:
+            raise Exception("Software cannot be installed")
+
+        self.software_components.append(software)
+        self.available_memory -= software.memory_consumption
+        self.available_capacity -= software.capacity_consumption
+
+    def uninstall(self, software: Software):
+        if software in self.software_components:
+            self.software_components.remove(software)
+
+
