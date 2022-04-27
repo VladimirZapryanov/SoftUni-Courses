097c2e64fb7c6da84a17b81e28ce0cd23e579b15
Vladimir <v.zapryanov89@gmail.com>
2022-04-27 23:25:37 +0300

    fix syntax

--- Programing_Basics_with_JS/conditionalStatementsLab/05.Number100...200.js
+++ Programing_Basics_with_JS/conditionalStatementsLab/05.Number100...200.js
@@ -3,7 +3,7 @@ function numbers(input) {
 
     if (number < 100) {
         console.log('Less than 100');
-    }else if (100 <= number && number <= 200) {
+    }else if (number >= 100 && number <= 200) {
         console.log('Between 100 and 200');
     }else {
         console.log('Greater than 200');
