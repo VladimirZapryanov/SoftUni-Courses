7eeb55f0cf07dc4dc4c80e7ebbfb7756284c7494
Vladimir <v.zapryanov89@gmail.com>
2022-05-10 12:07:26 +0300

    add new exercise

---
+++ Programing_Basics_with_JS/forLoopLab/02.NumbersN...1.js
@@ -0,0 +1,11 @@
+function numbers(input) {
+    number = Number(input[0]);
+
+    for (let i = number; i > 0; i--) {
+        console.log(i);
+    }
+}
+
+numbers(['2'])
+numbers(['3'])
+numbers(['5'])
