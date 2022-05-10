f6b1df88101dea2cc41872b499e322ed8baca8b6
Vladimir <v.zapryanov89@gmail.com>
2022-05-10 12:30:22 +0300

    add new exercise

---
+++ Programing_Basics_with_JS/forLoopLab/05.CharacterSequence.js
@@ -0,0 +1,11 @@
+function characterSequence(input) {
+    let text = input[0];
+    let length = text.length;
+
+    for (let i = 0; i < length; i++) {
+        console.log(text[i]);
+    }
+}
+
+characterSequence(['softuni'])
+characterSequence(['ice cream'])
