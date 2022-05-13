c780da03997c3a9bae9b4f21a98776e2a90c0c97
Vladimir <v.zapryanov89@gmail.com>
2022-05-13 12:20:26 +0300

    add new exercise

---
+++ Programing_Basics_with_JS/conditionalStatementsAdvancedMoreExercises/09.NumbersFromOneToTen.js
@@ -0,0 +1,25 @@
+// With for loop!!!
+
+function numbersFromOneToTen() {
+    let firstNumber = 1;
+    let lostNumber = 10;
+
+    for (i = firstNumber; i <= lostNumber; i++) {
+        console.log(i);
+    }
+}
+
+numbersFromOneToTen()
+
+// With while loop!!!
+
+function numbersFromOneToTenWhileLoop() {
+    let i = 1;
+
+    while (i <= 10) {
+        console.log(i);
+        i++
+    }
+}
+
+numbersFromOneToTenWhileLoop()
