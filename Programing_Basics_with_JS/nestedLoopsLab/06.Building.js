e9d9524d372e32ba59d1b71492dc259957102247
Vladimir <v.zapryanov89@gmail.com>
2022-05-22 19:02:26 +0300

    add new exercise

---
+++ Programing_Basics_with_JS/nestedLoopsLab/06.Building.js
@@ -0,0 +1,23 @@
+function building(input) {
+    let numbersOfFloor = Number(input[0]);
+    let numbersOfRooms = Number(input[1]);
+
+    for(let f = numbersOfFloor; f >= 1 ; f--) {
+        let result = '';
+
+        for(let r = 0; r < numbersOfRooms; r++){
+            
+            if(f === numbersOfFloor) {
+                result += `L${f}${r} `;
+            } else if (f % 2 === 1) {
+                result += `A${f}${r} `;
+            } else if (f % 2 == 0) {
+                result += `O${f}${r} `;
+            }
+        }
+
+        console.log(result);
+    }
+}
+
+building(['6', '4'])
