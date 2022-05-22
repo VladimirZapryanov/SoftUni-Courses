3d1037f69a194e02171aba6a3b5526c5e9274b36
Vladimir <v.zapryanov89@gmail.com>
2022-05-22 19:03:41 +0300

    add tests

--- Programing_Basics_with_JS/nestedLoopsLab/06.Building.js
+++ Programing_Basics_with_JS/nestedLoopsLab/06.Building.js
@@ -6,7 +6,7 @@ function building(input) {
         let result = '';
 
         for(let r = 0; r < numbersOfRooms; r++){
-            
+
             if(f === numbersOfFloor) {
                 result += `L${f}${r} `;
             } else if (f % 2 === 1) {
@@ -21,3 +21,5 @@ function building(input) {
 }
 
 building(['6', '4'])
+building(['9', '5'])
+building(['4', '4'])
