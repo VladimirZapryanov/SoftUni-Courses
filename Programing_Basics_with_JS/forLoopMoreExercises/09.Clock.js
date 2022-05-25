8d6fbcb8fa0d83e39d1637d1473a3a2bca52239a
Vladimir <v.zapryanov89@gmail.com>
2022-05-25 16:14:20 +0300

    add new exercise

---
+++ Programing_Basics_with_JS/forLoopMoreExercises/09.Clock.js
@@ -0,0 +1,9 @@
+function clock() {
+    for(let hours = 0; hours <= 23; hours++) {
+        for(let minutes = 0; minutes <= 59; minutes++) {
+            console.log(`${hours} : ${minutes}`);
+        }
+    }
+}
+
+clock()
