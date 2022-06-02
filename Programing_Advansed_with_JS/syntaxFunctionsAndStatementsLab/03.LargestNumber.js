4af390c9e826819a097093325e16fd6e3d4d5343
Vladimir <v.zapryanov89@gmail.com>
2022-06-02 12:18:25 +0300

    add new exercise

---
+++ Programing_Advansed_with_JS/syntaxFunctionsAndStatementsLab/03.LargestNumber.js
@@ -0,0 +1,8 @@
+function largestNumber(arg1, arg2, arg3) {
+    let theBiggestNumber = Math.max(arg1, arg2, arg3);
+
+    console.log(`The largest number is ${theBiggestNumber}.`);
+}
+
+largestNumber(5, -3, 16)
+largestNumber(-3, -5, -22.5)
