093521a705f5d9d062b06dd49cb3d1f443222a42
Vladimir <v.zapryanov89@gmail.com>
2022-06-05 13:01:36 +0300

    add new exercise

---
+++ Programing_Advansed_with_JS/arraysAndNestedArraysExercise/01.PrintAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,20 @@
+function printArray(array, delimiter) {
+    console.log(array.join(delimiter));
+}
+
+printArray(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-'
+)
+
+printArray(['How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_'
+)
