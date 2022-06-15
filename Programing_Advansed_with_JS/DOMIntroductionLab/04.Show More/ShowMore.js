0ae331025d8b49b99a3350ad0ccfd494d684518d
Vladimir <v.zapryanov89@gmail.com>
2022-06-15 14:52:18 +0300

    add new exercise

---
+++ Programing_Advansed_with_JS/DOMIntroductionLab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let text = document.getElementById('text');
+    let readMore = document.getElementById('more');
+
+    readMore.style.display = 'none';
+    text.style.display = 'inline';
+}
