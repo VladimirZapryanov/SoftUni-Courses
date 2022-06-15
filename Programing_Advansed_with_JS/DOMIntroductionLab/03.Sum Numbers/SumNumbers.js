b3253b61a63610408699a80ddcfdb1ad7f7fdf9f
Vladimir <v.zapryanov89@gmail.com>
2022-06-15 14:29:50 +0300

    add new exercise

---
+++ Programing_Advansed_with_JS/DOMIntroductionLab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,8 @@
+function calc() {
+    let firstNumber = Number(document.getElementById('num1').value);
+    let secondNumber = Number(document.getElementById('num2').value);
+    
+    let sum = firstNumber + secondNumber;
+
+    document.getElementById('sum').value = sum;
+}
