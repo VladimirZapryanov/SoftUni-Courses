6f410112bbce3d6dc313ab5154ae3459da46916f
Vladimir <v.zapryanov89@gmail.com>
2022-06-15 16:19:23 +0300

    add new exercise

---
+++ Programing_Advansed_with_JS/DOMIntroductionLab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -0,0 +1,14 @@
+function extract(content) {
+    let text = document.getElementById(content).textContent;
+    let result = '';
+
+    let pattern = /\((.+?)\)/g;
+
+    let match = pattern.exec(text);
+    while (match != null) {
+        result += match[1] + '; '; 
+        match = pattern.exec(text);
+    }
+
+    return result;
+}
