3115735b1fd3ef8274f0c3f3e2aec7ed0ed198d6
Vladimir <v.zapryanov89@gmail.com>
2022-07-14 17:02:14 +0300

    add new exercise

---
+++ Algorithms_with_Python/recursion_and_backtracking_lab/02._recursive_factorial.py
@@ -0,0 +1,9 @@
+def recursive_factorial(current_element):
+    if current_element == 1:
+        return current_element
+    return current_element * recursive_factorial(current_element - 1)
+
+
+num = int(input())
+
+print(recursive_factorial(num))
