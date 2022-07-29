4a4632a568469a1a50d231ed1808001c9fad34fc
Vladimir <v.zapryanov89@gmail.com>
2022-07-29 11:34:43 +0300

    add new exercise

---
+++ Algorithms_with_Python/searching_and_sorting_algorithms/02._binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+    while left <= right:
+        mid_index = (left + right) // 2
+        mid_element = numbers[mid_index]
+
+        if mid_element == target:
+            return mid_index
+        if mid_element < target:
+            left = mid_index + 1
+        else:
+            right = mid_index - 1
+
+    return - 1
+
+
+numbers = [int(x) for x in input().split(' ')]
+target = int(input())
+
+print(binary_search(numbers, target))
