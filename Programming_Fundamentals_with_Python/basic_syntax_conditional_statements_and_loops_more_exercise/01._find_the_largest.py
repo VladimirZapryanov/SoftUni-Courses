9596452fd8894ac3f14264b95eeae22717d7d07e
Vladimir <v.zapryanov89@gmail.com>
2022-11-15 20:21:49 +0200

    add new exercise

--- Programming_Fundamentals_with_Python/basic_syntax_conditional_statements_and_loops_more_exercise/01._find_the_largest.py
+++ Programming_Fundamentals_with_Python/basic_syntax_conditional_statements_and_loops_more_exercise/01._find_the_largest.py
@@ -1,2 +1,14 @@
-number = int(input())
+number_str = list(input())
+number_range = len(number_str)
 
+for _ in range(number_range):
+    for n1 in range(number_range):
+        if n1 + 1 == number_range:
+            break
+        first_number = int(number_str[n1])
+        second_number = int(number_str[n1 + 1])
+
+        if second_number > first_number:
+            number_str[n1], number_str[n1 + 1] = number_str[n1 + 1], number_str[n1]
+
+print(''.join(number_str))
