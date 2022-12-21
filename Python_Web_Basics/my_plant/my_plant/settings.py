c5624ba8f6bc1327eaed1523ce69dbe0a65c547e
Vladimir Zapryanov <90334985+VladimirZapryanov@users.noreply.github.com>
2022-12-21 17:48:36 +0200

    Update settings.py
    delete secret key

--- Python_Web_Basics/my_plant/my_plant/settings.py
+++ Python_Web_Basics/my_plant/my_plant/settings.py
@@ -8,8 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r&jp9_dv9d435!zi02y=y0y0kn#-g*!ip9^7wyk$7x_l^v*jsy'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
