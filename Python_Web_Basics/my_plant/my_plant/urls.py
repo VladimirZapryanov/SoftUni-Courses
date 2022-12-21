54bad10e88963d5a745298a1c232d867c728e015
Vladimir <v.zapryanov89@gmail.com>
2022-12-21 17:39:16 +0200

    add new project

---
+++ Python_Web_Basics/my_plant/my_plant/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = (
+    path('admin/', admin.site.urls),
+    path('', include('my_plant.my_plant_app.urls')),
+)
