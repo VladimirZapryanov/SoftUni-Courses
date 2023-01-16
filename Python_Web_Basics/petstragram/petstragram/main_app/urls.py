238ee5a28e88bae34c92794d483b5d18239425f1
Vladimir <v.zapryanov89@gmail.com>
2023-01-16 21:42:35 +0200

    add new urls

--- Python_Web_Basics/petstragram/petstragram/main_app/urls.py
+++ Python_Web_Basics/petstragram/petstragram/main_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from petstragram.main_app.views import home, dashboard, profile_details, photo_details
+from petstragram.main_app.views import show_home, show_dashboard, show_profile, show_pet_photo_details, like_pet_photo
 
-urlpatterns = [
-    path('', home, name='index'),
-    path('dashboard/', dashboard, name='dashboard'),
-    path('profile/', profile_details, name='profile details'),
-    path('photo/details/<int:pk>', photo_details, name='photo details')
-]
+urlpatterns = (
+    path('', show_home, name='index'),
+    path('dashboard/', show_dashboard, name='dashboard'),
+    path('profile/', show_profile, name='profile'),
+    path('photo/details/<int:pk>/', show_pet_photo_details, name='pet photo details'),
+    path('photo/like/<int:pk>/', like_pet_photo, name='like pet photo'),
+)
 
