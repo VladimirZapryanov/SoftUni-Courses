bec89e61e395250223d1f56e697db84f7aa53248
Vladimir <v.zapryanov89@gmail.com>
2023-01-17 18:45:33 +0200

    add new admin part

--- Python_Web_Basics/petstragram/petstragram/main_app/admin.py
+++ Python_Web_Basics/petstragram/petstragram/main_app/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from petstragram.main_app.models import Profile, Pet
+
+from petstragram.main_app.models import Profile, Pet, PetPhoto
+
+
+class PetInLineAdmin(admin.StackedInline):
+    model = Pet
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    inlines = (PetInLineAdmin,)
+    list_display = ('first_name', 'last_name')
 
 
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type')
+
+
+@admin.register(PetPhoto)
+class PetPhotoAdmin(admin.ModelAdmin):
     pass
