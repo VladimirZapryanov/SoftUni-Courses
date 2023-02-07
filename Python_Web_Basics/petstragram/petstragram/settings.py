39a71d1822cbac6630bfd8868395d9091f4b28cd
Vladimir <v.zapryanov89@gmail.com>
2023-02-07 18:27:33 +0200

    d

--- Python_Web_Basics/petstragram/petstragram/settings.py
+++ Python_Web_Basics/petstragram/petstragram/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-slwn%7f+=$v$165i(_&p@vk$ed9avaoi%8vt1=dqto@5!@-q38'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
